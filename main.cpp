748f57ee862375845b933f1a0b1ed1fed0f26e94
Dawid Dziedzic <dziedzic@mda.pl>
2018-11-14 15:59:13 +0100

    Initial commit, some data

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+     
+    /*
+        It appears that if you put single string in
+        multiple lines then it won't work
+    */
+    cout << "Learning project. Nothing interesting here, if you know C++ already, keep moving"<<endl;
+
+    /*
+        Enum declares type color, in which you can
+        declare only values red, grren, blue and creates
+        new variable of this type named c
+    */
+    enum color {red, green, blue} c;
+
+    /*
+        New variable types:
+        void - absence of type
+        wchar_t - wide character type
+
+        rvalue and lvalue:
+        rvalue - can stand at the left and the right
+            side of an assignment (variable),
+        lvalue - can stand only at the right side of
+            an assignment (constant, like 20)
+    */
+
+
+}
