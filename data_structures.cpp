4046b6ad19110c3b8bc3e035693199756e46471c
Dawid Dziedzic <dziedzic@mda.pl>
2018-11-23 15:45:54 +0100

    Structures

--- data_structures.cpp
+++ data_structures.cpp
@@ -11,6 +11,9 @@ struct Book {
     int book_id;
 };
 
+// structure can be passed to function as an argument like this:
+void printBook( struct Book book);
+
 void dataStructuresPresentation(){
     struct Book Book1;
 
@@ -19,5 +22,21 @@ void dataStructuresPresentation(){
     strcpy(Book1.subject, "C++ Programming");
     Book1.book_id = 123456;
 
-    cout << Book1.book_id << endl;
+    printBook(Book1);
+
+    //you can define pointers to stucture
+    struct Book *struct_pointer;
+    struct_pointer = &Book1;
+
+    //you can access member of structure by pointer with -> operator
+    cout << struct_pointer->subject << endl;
+
+    // typedef
+}
+
+void printBook(struct Book book){
+    cout << "Book title: " << book.title << endl;
+    cout << "Book author: " << book.author << endl;
+    cout << "Book subject: " << book.subject << endl;
+    cout << "Book id: " << book.book_id << endl;
 }
